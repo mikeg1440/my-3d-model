4e2133ec66d1623122a39ceaca8fb01f4f933415
mikeg1440 <gaudreaum01@gmail.com>
2022-09-09 19:01:11 -0400

    Add new classes to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import ModelScene from './ModelScene';
 function App() {
 
   return (
-    <div className="App">
+    <div className="app">
+      <div className='main-header glow'>My 3D Model</div>
       <ModelScene />
     </div>
   )
