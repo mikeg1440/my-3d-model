020fc5a6bae88c1e83496900768169911fdd83c5
mikeg1440 <gaudreaum01@gmail.com>
2022-09-09 19:02:47 -0400

    Add banner and github link below the avatar model

--- src/components/ModelScene.jsx
+++ src/components/ModelScene.jsx
@@ -6,21 +6,34 @@ import Avatar from './Avatar.jsx';
 
 export default function ModelScene() {
   return (
-    <Canvas 
+    <>
+      <Canvas 
         camera={{ position: [0, 4, 15], fov: 15 }}
-      style={{
+        style={{
           backgroundColor: '#242424',
           width: 'fit-content',
           height: '300px',
-    }}>
+      }}>
 
-      <ambientLight intensity={1.25} />  
-      <ambientLight intensity={0.1} />  
-      <directionalLight intensity={1.4} />
-      <Suspense fallback={null}>
-        <Avatar />
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
+        <ambientLight intensity={1.25} />  
+        <ambientLight intensity={0.1} />  
+        <directionalLight intensity={1.4} />
+        <Suspense fallback={null}>
+          <Avatar />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+
+      <div className="banner subheader">
+        <p>
+          Check out my Github!
+        </p>
+      </div>
+
+      <a className='github-link' target='_blank' href="https://github.com/mikeg1440">
+        <img className='github-gif' src="https://octodex.github.com/images/daftpunktocat-guy.gif" alt="Github octokitty with a space helmet" />
+      </a>
+    
+    </>
   )
 }
